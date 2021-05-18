16bbef609ed671e3babded4f38fb9a1fdde8aa48
dolan <akashjaiswar9822@gmail.com>
2021-05-18 18:53:48 +0530

    made a few error corrections

---
+++ source/implementation/x86_64/boot/header.asm
@@ -0,0 +1,12 @@
+section .multiboot_head
+header_start:
+
+    dd 0xe85250d6   ;multiboot2
+    dd 0    ;protected mode i386
+    dd header_end - header_start
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    dw 0
+    dw 0
+    dd 8
+
+header_end:
